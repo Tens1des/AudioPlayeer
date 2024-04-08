ebd9fa0312d015c5143490fb06557e85c210f509
Roman <Romankotov3213@gmail.com>
2024-04-08 18:31:13 +0300

    Second Commit

--- AudioPlayeerUIKit/ViewController.swift
+++ AudioPlayeerUIKit/ViewController.swift
@@ -6,14 +6,62 @@
 //
 
 import UIKit
-
+import AVFoundation
 class ViewController: UIViewController {
-
+   //MARK: -Outlet
+    @IBOutlet weak var playButton: UIButton!
+    @IBOutlet weak var sliderDuration: UISlider!
+    @IBOutlet weak var sliderSound: UISlider!
+    //MARK: - Konstant
+    var player = AVAudioPlayer()
+    private var shouldPlay:Bool = false
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        do {
+            if let audioPath = Bundle.main.path(forResource: "ROCKET - CSO", ofType: "mp3") {
+                try player = AVAudioPlayer(contentsOf: URL(fileURLWithPath: audioPath))
+                self.sliderDuration.maximumValue = Float(player.duration)
+            }
+        } catch {
+            print("Error")
+        }
     }
 
-
+    
+    //MARK: - Action
+    @IBAction func buttonDidToch(_ sender: Any) {
+        shouldPlay = !shouldPlay
+        if shouldPlay {
+            play()
+        }else {
+            pause()
+        }
+        
+        func play() {
+            let pauseImage = UIImage(systemName: "pause.fill")
+            playButton.setImage(pauseImage, for: .normal)
+            self.player.stop()
+        }
+        
+        func pause () {
+            let playImage = UIImage(systemName: "play.fill")
+            playButton.setImage(playImage, for: .normal)
+            self.player.play()
+        }
+    }
+    
+    @IBAction func changeSlider(_ sender: UISlider) {
+        if sender == sliderDuration{
+            self.player.currentTime = TimeInterval(sender.value)
+        }
+    }
+    
+    @IBAction func sliderSoundAction(_ sender: Any) {
+        self.player.volume = self.sliderSound.value
+    }
+    
+    
 }
 
